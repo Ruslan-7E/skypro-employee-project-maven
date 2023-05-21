9a4d75a2c587872d66cb59bffb7377b6f9430ec6
Ruslan Mukhametshin <ruslanow597@gmail.com>
2023-05-21 21:39:23 +0300

    Initial commit

---
+++ src/main/java/pro/sky/skyproemployeebookcoursework/SkyproEmployeeProjectApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproemployeebookcoursework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproEmployeeProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproEmployeeProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/skyproemployeebookcoursework/SkyproEmployeeProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.skyproemployeebookcoursework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyproEmployeeProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
